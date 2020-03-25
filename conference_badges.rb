8e0bd285f36d5f4c601ee6d1e735c19c4ebd1206
Ray Alva <rayalva407@gmail.com>
2020-03-25 19:44:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,9 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(array)
-  
+  new_array = []
+  array.each do |name|
+    new_array << "Hello, my name is #{name}."
 end
 
 def assign_rooms(speakers)
